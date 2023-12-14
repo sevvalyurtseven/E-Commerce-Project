702740722d618687d6429452f708dd7252769da1
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-14 23:54:47 +0300

    shop is done

--- my-react-project/src/pages/ProductListPage.js
+++ my-react-project/src/pages/ProductListPage.js
@@ -1,11 +1,13 @@
 import Clients from "../components/Clients";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import Shop from "../layouts/Shop";
 
 function ProductListPage() {
     return (
       <div>
       <NavBar />
+      <Shop />
        <Clients />
         <Footer />
       </div>
