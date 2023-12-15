2667d3c274aac0f2aae7b5b4c463f0e38cdb312c
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-15 17:36:04 +0300

    slider is okay

--- my-react-project/src/components/MainSlider.js
+++ my-react-project/src/components/MainSlider.js
@@ -1,20 +1,20 @@
+import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Header from "../layouts/Header"
+import Header from "../layouts/Header";
 import HeaderCopy from "../layouts/HeaderCopy";
-
-
 
 const MainSlider = () => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
   };
+
   return (
     <div className="mx-auto">
       <Slider {...settings}>
@@ -25,8 +25,31 @@ const MainSlider = () => {
           <HeaderCopy />
         </div>
       </Slider>
+
+      <style jsx>{`
+        .slick-prev,
+        .slick-next {
+          font-size: 60px;
+          padding: 20px;
+          position: absolute;
+          top: 50%;
+          transform: translateY(-50%);
+          z-index: 1;
+        }
+
+        .slick-prev {
+          left: 50px;
+        }
+
+        .slick-next {
+          right: 140px;
+        }
+      `}</style>
     </div>
   );
 };
 
 export default MainSlider;
+
+
+
