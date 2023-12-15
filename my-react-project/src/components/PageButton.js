320758536e4cd1767d09767acfe2ddbafb0c9c38
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-15 04:02:21 +0300

    product list page is done

---
+++ my-react-project/src/components/PageButton.js
@@ -0,0 +1,43 @@
+const PageButton = () => {
+    const clickHandler = (e) => {
+      e.target.classList.add("hover:bg-[#23A6F0]");
+    };
+  
+    return (
+      <div>
+        <button
+          className="text-base tracking-wider py-6 px-6 border-solid border-[1.346px] border-[#E8E8E8]  text-[#BDBDBD] bg-[#F3F3F3] shadow-[#BDBDBD] font-bold"
+          onClick={clickHandler}
+        >
+          First
+        </button>
+        <button
+          className="text-base py-6 tracking-wider px-5 border-solid border-[1.346px] border-[#E8E8E8]  shadow-[#BDBDBD] "
+          onClick={clickHandler}
+        >
+          1
+        </button>
+        <button
+          className="text-base py-6 px-5 tracking-wider border-solid border-[1.346px] border-[#E8E8E8] shadow-[#BDBDBD] "
+          onClick={clickHandler}
+        >
+          2
+        </button>
+        <button
+          className="text-base py-6 px-5 tracking-wider border-solid border-[1.346px] border-[#E8E8E8]  shadow-[#BDBDBD] "
+          onClick={clickHandler}
+        >
+          3
+        </button>
+        <button
+          className="text-base py-6 px-6 tracking-wider border-solid border-[1.346px] border-[#E8E8E8]   shadow-[#BDBDBD] font-bold"
+          onClick={clickHandler}
+        >
+          Next
+        </button>
+      </div>
+    );
+  };
+  
+  export default PageButton;
+  
--- my-react-project/src/pages/ProductListPage.js
+++ my-react-project/src/pages/ProductListPage.js
@@ -1,6 +1,7 @@
 import Clients from "../components/Clients";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import ProductPageCard from "../components/ProductPageCard";
 import Shop from "../layouts/Shop";
 
 function ProductListPage() {
@@ -8,6 +9,7 @@ function ProductListPage() {
       <div>
       <NavBar />
       <Shop />
+      <ProductPageCard />
        <Clients />
         <Footer />
       </div>
