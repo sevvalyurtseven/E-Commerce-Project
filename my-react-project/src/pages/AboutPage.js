266892bf1322b8e0a34d73ea8af6322fbd7b7484
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-18 11:19:24 +0300

    companies is okay

--- my-react-project/src/pages/AboutPage.js
+++ my-react-project/src/pages/AboutPage.js
@@ -1,20 +1,20 @@
+import Companies from "../components/About/Companies";
 import HeaderContent from "../components/About/HeaderContent";
 import StatsSection from "../components/About/StatsSection";
 import Team from "../components/About/Team";
-import Clients from "../components/Clients";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
-function AboutPage () {
-    return (
-        <div>
-            <NavBar />
-            <HeaderContent/>
-            <StatsSection/>
-            <Team />
-            <Clients/>
-            <Footer/>
-        </div>
-    )
+function AboutPage() {
+  return (
+    <div>
+      <NavBar />
+      <HeaderContent />
+      <StatsSection />
+      <Team />
+      <Companies />
+      <Footer />
+    </div>
+  );
 }
 export default AboutPage;
