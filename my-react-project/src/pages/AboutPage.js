1ad3c1c89bf6b601c4f4c388255c66825b96e5f9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-18 06:36:13 +0300

    about page's header content is done

--- my-react-project/src/pages/AboutPage.js
+++ my-react-project/src/pages/AboutPage.js
@@ -1,9 +1,11 @@
+import HeaderContent from "../components/About/HeaderContent";
 import Clients from "../components/Clients";
 import Footer from "../components/Footer";
 
 function AboutPage () {
     return (
         <div>
+            <HeaderContent/>
             <Clients/>
             <Footer/>
         </div>
