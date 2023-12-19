527e6589728fec77da61f13dec1419b6598bdd34
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-19 20:05:45 +0300

    product header changing with flex layout

--- my-react-project/src/components/Product/ProductHeader.js
+++ my-react-project/src/components/Product/ProductHeader.js
@@ -15,7 +15,7 @@ import kucuk2 from "../../Assets/PRODUCT/kucuk2.png";
 
 function ProductHeader() {
   return (
-    <div className="bg-[#FAFAFA] px-44 relative">
+    <div className="bg-[#FAFAFA] px-44">
       <div className="flex items-center justify-start gap-3 py-10">
         <h4 className=" text-slate-800 text-sm font-bold leading-normal tracking-wider">
           Home
@@ -28,70 +28,75 @@ function ProductHeader() {
           Shop
         </h4>
       </div>
-      <img src={sarikoltuk} />
-      <div className="flex gap-4 pt-4">
-        <img src={kucuk1} />
-        <img src={kucuk2} />
-      </div>
-      <div className="absolute top-[17%] right-[15rem]">
-        <h3 className="text-slate-800 text-xl font-medium leading-[30px] tracking-wider">
-          Floating Phone
-        </h3>
-        <div className="flex items-center gap-1 py-4 ">
-          <FontAwesomeIcon icon={faStar} className="text-[#F3CD03]" />
-          <FontAwesomeIcon icon={faStar} className="text-[#F3CD03]" />
-          <FontAwesomeIcon icon={faStar} className="text-[#F3CD03]" />
-          <FontAwesomeIcon icon={faStar} className="text-[#F3CD03]" />
-          <FontAwesomeIcon icon={emptyStar} className="text-[#F3CD03]" />
-
-          <p className="text-neutral-500 text-sm font-bold leading-normal tracking-wider">
-            10 Reviews
-          </p>
+      <div className="flex justify-start">
+        <div className="flex">
+          <img src={sarikoltuk} />
         </div>
-        <div className="flex flex-col">
-          <h4 className=" text-slate-800 text-2xl font-bold leading-loose tracking-wider">
-            $1,139.33
-          </h4>
-          <div className="flex gap-2">
-            <h5 className="text-neutral-500 text-sm font-bold leading-normal tracking-wider">
-              Availability :
-            </h5>
-            <h5 className="text-sky-500 text-sm font-bold leading-normal tracking-wider">
-              In Stock{" "}
-            </h5>
-          </div>
-          <div className="py-10">
-            <p className="text-zinc-500 text-sm font-normal leading-tight tracking-wider">
-              Met minim Mollie non desert Alamo est sit cliquey dolor <br />
-              do met sent. RELIT official consequent door ENIM RELIT Mollie.{" "}
-              <br />
-              Excitation venial consequent sent nostrum met.
+
+        <div className="flex-flex-col  px-16">
+          <h3 className="text-slate-800 text-xl font-medium leading-[30px] tracking-wider">
+            Floating Phone
+          </h3>
+          <div className="flex items-center gap-1 py-4 ">
+            <FontAwesomeIcon icon={faStar} className="text-[#F3CD03]" />
+            <FontAwesomeIcon icon={faStar} className="text-[#F3CD03]" />
+            <FontAwesomeIcon icon={faStar} className="text-[#F3CD03]" />
+            <FontAwesomeIcon icon={faStar} className="text-[#F3CD03]" />
+            <FontAwesomeIcon icon={emptyStar} className="text-[#F3CD03]" />
+
+            <p className="text-neutral-500 text-sm font-bold leading-normal tracking-wider">
+              10 Reviews
             </p>
           </div>
-          <hr></hr>
-          <div className="py-10">
-            <img src={colors} />
-          </div>
+          <div className="flex flex-col">
+            <h4 className=" text-slate-800 text-2xl font-bold leading-loose tracking-wider">
+              $1,139.33
+            </h4>
+            <div className="flex gap-2">
+              <h5 className="text-neutral-500 text-sm font-bold leading-normal tracking-wider">
+                Availability :
+              </h5>
+              <h5 className="text-sky-500 text-sm font-bold leading-normal tracking-wider">
+                In Stock{" "}
+              </h5>
+            </div>
+            <div className="py-10">
+              <p className="text-zinc-500 text-sm font-normal leading-tight tracking-wider">
+                Met minim Mollie non desert Alamo est sit cliquey dolor <br />
+                do met sent. RELIT official consequent door ENIM RELIT Mollie.{" "}
+                <br />
+                Excitation venial consequent sent nostrum met.
+              </p>
+            </div>
+            <hr></hr>
+            <div className="py-10">
+              <img src={colors} />
+            </div>
 
-          <div className="flex items-center gap-4">
-            <button className="text-white text-sm font-bold leading-normal tracking-wider border border-solid rounded-md bg-[#23A6F0] px-6 py-3">
-              Select Options
-            </button>
+            <div className="flex items-center gap-4">
+              <button className="text-white text-sm font-bold leading-normal tracking-wider border border-solid rounded-md bg-[#23A6F0] px-6 py-3">
+                Select Options
+              </button>
 
-            <FontAwesomeIcon
-              icon={faHeart}
-              className="border border-solid rounded-full border-[#E8E8E8] p-3"
-            />
-            <FontAwesomeIcon
-              icon={faCartShopping}
-              className="border border-solid rounded-full border-[#E8E8E8] p-3"
-            />
-            <FontAwesomeIcon
-              icon={faEye}
-              className="border border-solid rounded-full border-[#E8E8E8] p-3"
-            />
+              <FontAwesomeIcon
+                icon={faHeart}
+                className="border border-solid rounded-full border-[#E8E8E8] p-3"
+              />
+              <FontAwesomeIcon
+                icon={faCartShopping}
+                className="border border-solid rounded-full border-[#E8E8E8] p-3"
+              />
+              <FontAwesomeIcon
+                icon={faEye}
+                className="border border-solid rounded-full border-[#E8E8E8] p-3"
+              />
+            </div>
           </div>
         </div>
+      </div>
+      <div className="flex gap-4 py-4">
+        <img src={kucuk1} />
+        <img src={kucuk2} />
       </div>
     </div>
   );
