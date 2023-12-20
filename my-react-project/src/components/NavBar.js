f9f4005714747fdfb3e5c8a907c8a711a3cb27d6
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-20 18:18:20 +0300

    navbar is a bit more responsive now

--- my-react-project/src/components/NavBar.js
+++ my-react-project/src/components/NavBar.js
@@ -17,46 +17,52 @@ import { faEnvelope, faHeart } from "@fortawesome/free-regular-svg-icons";
 
 import { Link } from "react-router-dom";
 
-function NavBar () {
-    return (
-        <div className="font-bold">
-            <div className="bg-[#252B42] text-white font-bold flex justify-between items-center py-4 px-6">
-                <div className="flex gap-[2rem]">
-                    <div className="flex gap-[0.4rem] items-center">
-                    <FontAwesomeIcon icon={faPhone} />
-                    <p>(225) 555-0118</p>
-                    </div>
-                    <div className="flex gap-[0.4rem] items-center">
-                        <FontAwesomeIcon icon={faEnvelope}/>
-                        <p>michelle.rivera@example.com</p>
-                    </div>
-                    </div>
-                    <p>Follow Us and get a chance to win 80% off</p>
-                    <div className="flex gap-4 items-center">
-                        <p>Follow Us :</p>
-                        <FontAwesomeIcon icon={faInstagram}/>
-                        <FontAwesomeIcon icon={faYoutube}/>
-                        <FontAwesomeIcon icon={faFacebook}/>
-                        <FontAwesomeIcon icon={faTwitter}/>
-                    </div>
-            </div>
-            <div className="flex justify-between items-center py-6 px-8">
-        <h1 className="text-2xl">Bandage</h1>
-        <div className="flex justify-between w-[80%]">
-        <nav className="text-[#737373] flex gap-[1.5rem]">
+function NavBar() {
+  return (
+    <div className="font-bold">
+      <div className="xl:flex  hidden flex-wrap justify-between text-white text-sm font-bold leading-normal tracking-wider bg-[#252B42] px-8 py-5 ">
+        <div className="flex items-center gap-8">
+          <div className="flex items-center gap-1">
+            <FontAwesomeIcon icon={faPhone} />
+            <p>(225) 555-0118</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <p>michelle.rivera@example.com</p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <p>Follow Us and get a chance to win 80% off</p>
+        </div>
+        <div className="flex items-center gap-3">
+          <p>Follow Us :</p>
+          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faYoutube} />
+          <FontAwesomeIcon icon={faFacebook} />
+          <FontAwesomeIcon icon={faTwitter} />
+        </div>
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center justify-between pb-4 pt-4 px-14">
+        <div className="flex items-center">
+          <h2 className="text-slate-800 text-2xl font-bold leading-loose tracking-wider">
+            Bandage
+          </h2>
+        </div>
+        <nav className="flex flex-col sm:flex-row items-center  gap-4 text-center text-neutral-500 font-bold leading-normal tracking-wider">
           <Link to="/">Home</Link>
           <Link className="text-[#252B42] font-medium" to="/productlist">
             Shop
-            <FontAwesomeIcon icon={faAngleDown} style={{ marginLeft: '0.6rem' }}/>
+            <FontAwesomeIcon icon={faAngleDown} className="pl-3" />
           </Link>
           <Link to="/about">About</Link>
           <Link to="/">Blog</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/">Pages</Link>
         </nav>
-        <div className="text-[#23A6F0] flex gap-[1.7rem]">
+        <div className=" text-sky-500  font-bold leading-normal tracking-tight xl:flex hidden items-center gap-10 ">
           <Link to="/">
-            <FontAwesomeIcon icon={faUser} style={{ marginRight: '0.2rem' }} />
+            <FontAwesomeIcon icon={faUser} />
             <span> Login</span>
             <span> /</span>
             <span> Register</span>
@@ -72,9 +78,8 @@ function NavBar () {
           </Link>
         </div>
       </div>
-        </div>
-        </div>
-    )
+    </div>
+  );
 }
 
 export default NavBar;
