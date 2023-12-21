efe49399e7fc2283b58c72bd8bfa0ae64074917c
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-21 20:38:18 +0300

    contact page makes responsive for mobile

--- my-react-project/src/components/Contact/ContactContent.js
+++ my-react-project/src/components/Contact/ContactContent.js
@@ -14,43 +14,62 @@ function ContactContent() {
         We help small businesses <br />
         with big ideas
       </h2>
-      <div className="flex gap-20 pt-32">
+      <div className="flex flex-wrap sm:flex-nowrap justify-center gap-20 pt-32">
         {contactCards.map((image, index) => (
-          <div key={index} className={`flex flex-col items-center gap-8 py-14 ${
-            index === 1 ? "bg-[#252B42]" : ""
-          }`}>
+          <div
+            key={index}
+            className={`flex flex-col items-center gap-8 py-14 ${
+              index === 1 ? "bg-[#252B42]" : ""
+            }`}
+          >
             <img src={image} alt={`card ${index}`} />
             <div className="flex flex-col gap-4">
-              <p className={` text-center text-sm font-bold leading-normal tracking-wider ${
-            index === 1 ? "text-white" : "text-slate-800"
-          }`}>
-              georgia.young@example.com
+              <p
+                className={` text-center text-sm font-bold leading-normal tracking-wider ${
+                  index === 1 ? "text-white" : "text-slate-800"
+                }`}
+              >
+                georgia.young@example.com
               </p>
-              <p className={` text-center text-sm font-bold leading-normal tracking-wider ${
-            index === 1 ? "text-white" : "text-slate-800"
-          }`}>
-              georgia.young@ple.com
+              <p
+                className={` text-center text-sm font-bold leading-normal tracking-wider ${
+                  index === 1 ? "text-white" : "text-slate-800"
+                }`}
+              >
+                georgia.young@ple.com
               </p>
-              <h4 className={`text-center text-base font-bold  leading-normal tracking-wider ${
-            index === 1 ? "text-white" : "text-slate-800"
-          }`}>Get Support</h4>
-              
+              <h4
+                className={`text-center text-base font-bold  leading-normal tracking-wider ${
+                  index === 1 ? "text-white" : "text-slate-800"
+                }`}
+              >
+                Get Support
+              </h4>
+
               <div className="px-12 ">
-                <button className="border-[#23A6F0] border-2 rounded-full text-[#23A6F0] text-sm font-bold leading-normal tracking-wider px-8 py-3">Submit Request</button>
-              
+                <button className="border-[#23A6F0] border-2 rounded-full text-[#23A6F0] text-sm font-bold leading-normal tracking-wider px-8 py-3">
+                  Submit Request
+                </button>
+              </div>
             </div>
-          </div></div>
+          </div>
         ))}
-        
       </div>
       <div className="pt-10 flex flex-col items-center justify-center gap-6">
-      <img src = {ok} />
-      <h4 className=" text-slate-800 text-base font-bold leading-normal tracking-wider">WE Can't WAIT TO MEET YOU</h4>
-      <h2 className=" text-slate-800 text-6xl font-bold leading-[80px] tracking-wider">Let’s Talk</h2>
-      <div className="border border-solid bg-sky-500 px-10 py-4 rounded-md">
-      <button className="text-center text-white text-sm font-bold leading-snug tracking-wider">Try it free now</button>
-</div></div>
+        <img src={ok} />
+        <h4 className=" text-slate-800 text-base font-bold leading-normal tracking-wider">
+          WE Can't WAIT TO MEET YOU
+        </h4>
+        <h2 className=" text-slate-800 text-6xl font-bold leading-[80px] tracking-wider">
+          Let’s Talk
+        </h2>
+        <div className="border border-solid bg-sky-500 px-10 py-4 rounded-md">
+          <button className="text-center text-white text-sm font-bold leading-snug tracking-wider">
+            Try it free now
+          </button>
+        </div>
       </div>
+    </div>
   );
 }
 export default ContactContent;
--- my-react-project/src/components/Contact/ContactHeader.js
+++ my-react-project/src/components/Contact/ContactHeader.js
@@ -6,23 +6,23 @@ import linkedin from "../../Assets/TEAM/logos_linkedin-icon.svg";
 
 function ContactHeader() {
   return (
-    <div className="flex">
-      <div className="flex flex-col gap-8 p-40">
+    <div className="flex flex-col sm:flex-row">
+      <div className="flex flex-col gap-12 sm:gap-8 sm:p-40 p-20 justify-center sm:justify-start items-center sm:items-start">
         <h2 className="text-slate-800 text-base font-bold leading-normal tracking-wider">
           CONTACT US
         </h2>
-        <h3 className="text-slate-800 text-6xl font-bold leading-[80px] tracking-widert">
-          Get in touch <br />
+        <h3 className="text-slate-800 sm:text-6xl text-3xl font-bold sm:leading-[80px] leading-none text-center sm:text-start tracking-wider">
+          Get in touch <br className="hidden sm:block" />
           today!
         </h3>
-        <p className=" text-neutral-500 text-xl font-normal leading-[30px] tracking-wider">
-          We know how large objects will act, <br />
+        <p className=" text-neutral-500 text-xl font-normal leading-[30px] tracking-wider text-center sm:text-start">
+          We know how large objects will act, <br className="hidden sm:block" />
           but things on a small scale
         </p>
-        <p className="text-slate-800 text-2xl font-bold leading-loose tracking-wider">
+        <p className="text-slate-800 sm:text-2xl text-base font-bold leading-loose tracking-wider">
           Phone ; +451 215 215
         </p>
-        <p className="text-slate-800 text-2xl font-bold leading-loose tracking-wider">
+        <p className="text-slate-800 sm:text-2xl text-base font-bold leading-loose tracking-wider">
           Fax : +451 215 215
         </p>
         <div className="flex gap-11">
@@ -33,11 +33,12 @@ function ContactHeader() {
           <img src={faceebok} />
           <FontAwesomeIcon icon={faInstagram} className="text-3xl" />
           <img src={linkedin} />
-        </div>
+        </div> 
+        
       </div>
 
-      <div className="flex justify-end ">
-        <img src={family} />
+     <div className="flex justify-center items-center px-16 sm:px-0">
+        <img src={family}/>
       </div>
     </div>
   );
