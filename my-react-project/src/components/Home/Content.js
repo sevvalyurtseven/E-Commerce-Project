42e1037af57f674eb401777962f316fcbce2dedc
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-22 19:48:24 +0300

    padding added

--- my-react-project/src/components/Home/Content.js
+++ my-react-project/src/components/Home/Content.js
@@ -12,19 +12,19 @@ function Content() {
           
 
         </div>
-        <div className="flex flex-col gap-4 px-10">
+        <div className="flex flex-col gap-4 px-10 py-10">
           <h3 className="text-sky-500 text-base font-bold leading-normal tracking-wider">
             Featured Products
           </h3>
           <h1 className="text-slate-800 text-[40px] font-bold leading-[50px] tracking-wider">
             We love what we do
           </h1>
-          <p className=" text-neutral-500 text-sm font-normal font-['Montserrat'] leading-tight tracking-tight">
+          <p className=" text-neutral-500 text-sm font-normal leading-tight tracking-wider">
             Problems trying to resolve the conflict between <br className="hidden sm:block" />
             the two major realms of Classical physics: <br className="hidden sm:block" />
             Newtonian mechanics.{" "}
           </p>
-          <p className=" text-neutral-500 text-sm font-normal font-['Montserrat'] leading-tight tracking-tight">
+          <p className=" text-neutral-500 text-sm font-normal  leading-tight tracking-wider ">
             Problems trying to resolve the conflict between <br className="hidden sm:block" />
             the two major realms of Classical physics: <br className="hidden sm:block" />
             Newtonian mechanics
