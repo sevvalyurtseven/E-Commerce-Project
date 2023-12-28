d5ea04c82783f807ce4e64863ab7c4cbf7378bb1
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-28 15:18:11 +0300

    import shoppingCartReducer

--- my-react-project/src/store/reducers/store.js
+++ my-react-project/src/store/reducers/store.js
@@ -3,11 +3,13 @@ import { globalReducer } from "./globalReducer";
 import { productReducer } from "./productReducer";
 import { userReducer } from "./userReducer";
 import { applyMiddleware } from "redux";
+import { shoppingCartReducer } from "./shoppingCartReducer";
 
 export const reducers = combineReducers({
     global: globalReducer,
     user: userReducer,
     product: productReducer,
+    shoppingCart: shoppingCartReducer,
 });
 
 export const store = createStore(reducers, applyMiddleware(thunk, logger));
