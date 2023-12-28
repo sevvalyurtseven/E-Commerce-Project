beca68f944ab4cc30f92b4416200e0c8131edf90
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2023-12-28 15:08:53 +0300

    create productReducer

--- my-react-project/src/store/reducers/productReducer.js
+++ my-react-project/src/store/reducers/productReducer.js
@@ -1,1 +1,10 @@
-export const productReducer = (state, action) => {};
+const initialState = {
+    productList: [],
+    totalProductCount: 0,
+    pageCount: 0,
+    activePage: 0,
+    fetchState: "NOT_FETCHED",
+  };
+  export const productReducer = (state = initialState, action) => {
+    return state;
+  };
