1fc4cedc78312641df135e3a417765f681a981a2
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-02 16:58:28 +0300

    change user initialstate action and reducer

--- my-react-project/src/store/actions/userActions.js
+++ my-react-project/src/store/actions/userActions.js
@@ -1,6 +1,35 @@
+import { toast } from "react-toastify";
+import { AxiosInstance } from "../../api/axiosInstance";
+
 export const LOGIN_USER = "LOGIN_USER";
 export const LOGOUT_USER = "LOGOUT_USER";
 
 export const loginUser = (user) => ({ type: LOGIN_USER, payload: user });
 
 export const logoutUser = () => ({ type: LOGOUT_USER });
+
+export const userLogin = (data, history) => {
+  return (dispatch) => {
+    AxiosInstance.post("/login", data)
+      .then((response) => {
+        console.log(response.data);
+
+        dispatch(loginUser(response.data));
+        localStorage.setItem("token", response.data.token);
+        toast.success("Welcome back!");
+        history.push("/");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        toast.error("Error occurred: " + error.response.data.message);
+      });
+  };
+};
+
+export const userLogout = () => {
+  return (dispatch) => {
+    dispatch(logoutUser());
+    localStorage.removeItem("token");
+    console.log("Cikis yapildi");
+  };
+};
--- my-react-project/src/store/reducers/userReducer.js
+++ my-react-project/src/store/reducers/userReducer.js
@@ -7,14 +7,26 @@
 
 import { LOGIN_USER, LOGOUT_USER } from "../actions/userActions";
 
-const initialState = {};
+const initialState = {
+  user: {
+    name: null,
+    email: null,
+    token: null,
+    role_id: "3",
+  },
+  isLoggedIn: false,
+};
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_USER:
-      return { ...state, ...action.payload };
+      return { ...state, user: action.payload, isLoggedIn: true };
     case LOGOUT_USER:
-      return {};
+      return {
+        ...state,
+        user: {},
+        isLoggedIn: false,
+      };
     default:
       return state;
   }
