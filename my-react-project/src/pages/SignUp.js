3b5c8d4df837d9de05ce77970e211428a86aaf24
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-02 17:09:28 +0300

    add login page link to the sign up page

--- my-react-project/src/pages/SignUp.js
+++ my-react-project/src/pages/SignUp.js
@@ -1,16 +1,15 @@
 import React from "react";
 import RegistrationForm from "../components/RegistrationForm";
+import gravatar from "gravatar";
+
+//console.log(gravatar.url("customer@commerce.com", { s: "400" })); gravatar!
 
 const SignUp = () => {
-    return (
-      <div className="flex flex-col justify-center items-center  w-full sm:px-96 px-4 sm:py-20 py-10 ">
-        
-          <h2 className="font-bold text-2xl text-[#156691]">
-            Registration Form
-          </h2>
-          <RegistrationForm />
-        </div>
-      
-    );
-  };
-  export default SignUp;
+  return (
+    <div className="flex flex-col justify-center items-center  w-full sm:px-96 px-4 sm:py-20 py-10 ">
+      <h2 className="font-bold text-2xl text-[#156691]">Registration Form</h2>
+      <RegistrationForm />
+    </div>
+  );
+};
+export default SignUp;
