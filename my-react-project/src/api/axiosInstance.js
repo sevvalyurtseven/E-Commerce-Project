1e1b9810b0b7c7e3343f4f9aa0d74f960e6bcb6f
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-03 15:33:51 +0300

    axios instance using token in local storage

--- my-react-project/src/api/axiosInstance.js
+++ my-react-project/src/api/axiosInstance.js
@@ -1,10 +1,25 @@
 import axios from "axios";
 
 export const createAxiosInstance = () => {
-  return axios.create({
-    baseURL: "https://workintech-fe-ecommerce.onrender.com/",
-    headers: {},
-  });
+  const token = localStorage.getItem("token");
+
+  return token
+    ? axios.create({
+        baseURL: "https://workintech-fe-ecommerce.onrender.com/",
+        headers: {
+          Authorization: token,
+        },
+      })
+    : axios.create({
+        baseURL: "https://workintech-fe-ecommerce.onrender.com/",
+        headers: {},
+      });
 };
 
 export let AxiosInstance = createAxiosInstance();
+
+export const renewAxiosInstance = () => {
+  AxiosInstance = createAxiosInstance();
+};
+
+renewAxiosInstance();
