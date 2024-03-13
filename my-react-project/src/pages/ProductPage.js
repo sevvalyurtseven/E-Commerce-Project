b0f34c9556f54ce14e1e15c0d8383e4b83534c41
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-13 18:41:15 +0300

    editted

--- my-react-project/src/pages/ProductPage.js
+++ my-react-project/src/pages/ProductPage.js
@@ -1,22 +1,22 @@
+import { useSelector } from "react-redux";
 import Clients from "../components/Clients";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import BestSellerProduct from "../components/Product/BestSellerProduct";
 import ProductDescription from "../components/Product/ProductDescription";
+import { useParams } from "react-router-dom";
 import ProductHeader from "../components/Product/ProductHeader";
 
-function ProductPage(){
-    return (
-        <div>
-            <NavBar/>
-           
-            <ProductHeader/>
-            <ProductDescription/>
-            <BestSellerProduct/>
-            <Clients/>
-            <Footer/>
-            
-        </div>
-    )
+function ProductPage() {
+  return (
+    <div>
+      <NavBar />
+      <ProductHeader />
+      <ProductDescription />
+      <BestSellerProduct />
+      <Clients />
+      <Footer />
+    </div>
+  );
 }
 export default ProductPage;
