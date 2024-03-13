43047e6f8c71158f0d720bb13971a40df8fd5d3f
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-03-13 18:29:37 +0300

    product action ve reducer güncellendi

--- my-react-project/src/store/actions/productActions.js
+++ my-react-project/src/store/actions/productActions.js
@@ -1,29 +1,31 @@
 import { AxiosInstance } from "../../api/axiosInstance";
 
 export const SET_PRODUCT_LIST = "SET_PRODUCT_LIST";
+export const CLEAR_PRODUCT_LIST = "CLEAR_PRODUCT_LIST";
+export const SET_BEST_SELLERS = "SET_BEST_SELLERS";
 export const SET_TOTAL_PRODUCT_COUNT = "SET_TOTAL_PRODUCT_COUNT";
-export const SET_PAGE_COUNT = "SET_PAGE_COUNT";
-export const SET_ACTIVATE_PAGE = "SET_ACTIVATE_PAGE";
+export const SET_OFFSET = "SET_OFFSET";
 export const SET_FETCH_STATE = "SET_FETCH_STATE";
 
-export const setProductList = (productList) => ({
+export const setProductList = (products) => ({
   type: SET_PRODUCT_LIST,
-  payload: productList,
+  payload: products,
 });
 
+export const clearProductList = () => ({
+  type: CLEAR_PRODUCT_LIST,
+});
+export const setBestSellers = (products) => ({
+  type: SET_BEST_SELLERS,
+  payload: products,
+});
 export const setTotalProductCount = (count) => ({
   type: SET_TOTAL_PRODUCT_COUNT,
   payload: count,
 });
-
-export const setPageCount = (count) => ({
-  type: SET_PAGE_COUNT,
-  payload: count,
-});
-
-export const setActivatePage = (page) => ({
-  type: SET_ACTIVATE_PAGE,
-  payload: page,
+export const setOffset = (offset) => ({
+  type: SET_OFFSET,
+  payload: offset,
 });
 
 export const setFetchState = (fetchState) => ({
@@ -31,27 +33,43 @@ export const setFetchState = (fetchState) => ({
   payload: fetchState,
 });
 
-export const fetchProducts = (category, filter, sort) => {
+export const fetchProducts = (
+  category,
+  filter,
+  sort,
+  limit = 10,
+  offset = 0
+) => {
   return (dispatch) => {
-    const queryParams = {};
+    const queryParams = { limit, offset };
     if (category) queryParams.category = category;
     if (filter) queryParams.filter = filter;
     if (sort) queryParams.sort = sort;
 
     AxiosInstance.get("/products", { params: queryParams })
       .then((res) => {
-        console.log("urunler", res.data.products);
+        console.log("res.data", res.data);
         dispatch(setFetchState("FETCHING"));
-
-        setTimeout(() => {
-          dispatch(setProductList(res.data.products));
-          dispatch(setTotalProductCount(res.data.total));
-          dispatch(setFetchState("FETCHED"));
-        }, 1000);
+        dispatch(setProductList(res.data.products));
+        dispatch(setTotalProductCount(res.data.total));
+        dispatch(setFetchState("FETCHED"));
       })
       .catch((error) => {
         console.error("Error:", error);
         dispatch(setFetchState("FAILED"));
+      });
+  };
+};
+export const fetchBestSellers = () => {
+  return (dispatch) => {
+    const sort = "rating:desc";
+    AxiosInstance.get("/products", { params: { sort } })
+      .then((res) => {
+        console.log("res.data", res.data);
+        dispatch(setBestSellers(res.data.products));
+      })
+      .catch((error) => {
+        console.error("Error:", error);
       });
   };
 };
--- my-react-project/src/store/reducers/productReducer.js
+++ my-react-project/src/store/reducers/productReducer.js
@@ -1,29 +1,41 @@
 import {
   SET_PRODUCT_LIST,
+  CLEAR_PRODUCT_LIST,
+  SET_BEST_SELLERS,
   SET_TOTAL_PRODUCT_COUNT,
-  SET_PAGE_COUNT,
-  SET_ACTIVATE_PAGE,
+  SET_OFFSET,
   SET_FETCH_STATE,
 } from "../actions/productActions";
 
 const initialState = {
   productList: [],
+  bestSellers: [],
   totalProductCount: 0,
-  pageCount: 0,
-  activePage: 1,
+  offset: 0,
   fetchState: "NOT_FETCHED",
 };
 
 export const productReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_PRODUCT_LIST:
-      return { ...state, productList: action.payload };
+      return {
+        ...state,
+        productList: [...state.productList, ...action.payload],
+      };
+    case CLEAR_PRODUCT_LIST:
+      return {
+        ...state,
+        productList: [],
+      };
+    case SET_BEST_SELLERS:
+      return {
+        ...state,
+        bestSellers: action.payload,
+      };
     case SET_TOTAL_PRODUCT_COUNT:
       return { ...state, totalProductCount: action.payload };
-    case SET_PAGE_COUNT:
-      return { ...state, pageCount: action.payload };
-    case SET_ACTIVATE_PAGE:
-      return { ...state, activePage: action.payload };
+    case SET_OFFSET:
+      return { ...state, offset: action.payload };
     case SET_FETCH_STATE:
       return { ...state, fetchState: action.payload };
     default:
